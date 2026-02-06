2e7ebd9f830a7945c0520b9614bcf90f5e2d0a92
Olol1234 <muhammadazfar0239@gmail.com>
2026-02-06 18:22:14 +0800

    feat: make a general condition for turret to spawn and upgrdae

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -63,56 +63,108 @@ public class UpgradeManager : MonoBehaviour
             "Homing Missile Turret fires 1 additional missile"
         ));
 
+        allUpgrades.Add(new UpgradeData(
+            UpgradeType.LaserTurret,
+            "Laser Turret",
+            "Deploy a Laser Turret to assist you in battle"
+        ));
+
+        allUpgrades.Add(new UpgradeData(
+            UpgradeType.LaserTurretCooldown,
+            "-10% Turret Cooldown",
+            "Reduce Laser Turret's cooldown by 10%"
+        ));
+
+        allUpgrades.Add(new UpgradeData(
+            UpgradeType.LaserTurretDamagePercent,
+            "+20% Turret Damage",
+            "Increase Laser Turret's damage by 20%"
+        ));
+
+        allUpgrades.Add(new UpgradeData(
+            UpgradeType.LaserTurretDuration,
+            "+0.5s Turret Duration",
+            "Increase Laser Turret's active duration by 0.5 seconds"
+        ));
+
     }
 
-    // public List<UpgradeData> GetRandomUpgrades(int count)
-    // {
-    //     List<UpgradeData> pool = new List<UpgradeData>(allUpgrades);
-    //     List<UpgradeData> result = new List<UpgradeData>();
+    bool IsTurretUnlocked(UpgradeType type)
+    {
+        switch(type)
+        {
+            case UpgradeType.HomingMissileTurret:
+                return PlayerProfile.Instance.homingMissileTurretUnlocked;
 
-    //     for (int i = 0; i < count && pool.Count > 0; i++)
-    //     {
-    //         int index = Random.Range(0, pool.Count);
-    //         result.Add(pool[index]);
-    //         pool.RemoveAt(index); // ensures unique
-    //     }
+            case UpgradeType.LaserTurret:
+                return PlayerProfile.Instance.laserTurretUnlocked;
+        }
 
-    //     return result;
-    // }
+        return true;
+    }
+
+    bool IsTurretSpawned(UpgradeType type)
+    {
+        switch(type)
+        {
+            case UpgradeType.HomingMissileTurret:
+                return HomingMissileBrain.Instance != null;
+
+            case UpgradeType.LaserTurret:
+                return LaserTurretBrain.Instance != null;
+        }
+
+        return false;
+    }
+
+    bool IsTurretUpgrade(UpgradeType type, UpgradeType turretType)
+    {
+        if (turretType == UpgradeType.HomingMissileTurret)
+        {
+            return type == UpgradeType.HomingMissileTurretCooldown ||
+                type == UpgradeType.HomingMissileTurretDamagePercent ||
+                type == UpgradeType.HomingMissileTurretExtraMissile;
+        }
+
+        if (turretType == UpgradeType.LaserTurret)
+        {
+            return type == UpgradeType.LaserTurretCooldown ||
+                type == UpgradeType.LaserTurretDamagePercent ||
+                type == UpgradeType.LaserTurretDuration;
+        }
+
+        return false;
+    }
 
     public List<UpgradeData> GetRandomUpgrades(int count)
     {
         List<UpgradeData> pool = new List<UpgradeData>();
 
-        bool turretUnlocked = PlayerProfile.Instance.homingMissileTurretUnlocked;
-        bool turretSpawned = HomingMissileBrain.Instance != null;
+        // bool turretUnlocked = PlayerProfile.Instance.homingMissileTurretUnlocked;
+        // bool turretSpawned = HomingMissileBrain.Instance != null;
 
         foreach (var upgrade in allUpgrades)
         {
-            // ===== TURRET LOCKED =====
-            if (!turretUnlocked)
+            if (upgrade.type == UpgradeType.HomingMissileTurret ||
+                IsTurretUpgrade(upgrade.type, UpgradeType.HomingMissileTurret))
             {
-                if (upgrade.type == UpgradeType.HomingMissileTurret ||
-                    upgrade.type == UpgradeType.HomingMissileTurretCooldown ||
-                    upgrade.type == UpgradeType.HomingMissileTurretDamagePercent ||
-                    upgrade.type == UpgradeType.HomingMissileTurretExtraMissile)
-                    continue;
+                bool unlocked = IsTurretUnlocked(UpgradeType.HomingMissileTurret);
+                bool spawned = IsTurretSpawned(UpgradeType.HomingMissileTurret);
+
+                if (!unlocked) continue;
+                if (unlocked && !spawned && upgrade.type != UpgradeType.HomingMissileTurret) continue;
+                if (spawned && upgrade.type == UpgradeType.HomingMissileTurret) continue;
             }
 
-            // ===== TURRET UNLOCKED BUT NOT SPAWNED =====
-            if (turretUnlocked && !turretSpawned)
+            if (upgrade.type == UpgradeType.LaserTurret ||
+                IsTurretUpgrade(upgrade.type, UpgradeType.LaserTurret))
             {
-                if (upgrade.type == UpgradeType.HomingMissileTurretCooldown ||
-                    upgrade.type == UpgradeType.HomingMissileTurretDamagePercent ||
-                    upgrade.type == UpgradeType.HomingMissileTurretExtraMissile)
-                    continue;
-            }
+                bool unlocked = IsTurretUnlocked(UpgradeType.LaserTurret);
+                bool spawned = IsTurretSpawned(UpgradeType.LaserTurret);
 
-            // ===== TURRET SPAWNED =====
-            if (turretSpawned)
-            {
-                if (upgrade.type == UpgradeType.HomingMissileTurret)
-                    continue;
+                if (!unlocked) continue;
+                if (unlocked && !spawned && upgrade.type != UpgradeType.LaserTurret) continue;
+                if (spawned && upgrade.type == UpgradeType.LaserTurret) continue;
             }
 
             pool.Add(upgrade);
