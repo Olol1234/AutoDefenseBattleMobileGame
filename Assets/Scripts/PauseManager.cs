dfadd2b5ccb6145edede04429faf3ac4132338ae
Olol1234 <muhammadazfar0239@gmail.com>
2026-02-27 22:37:05 +0800

    feat: implement a general soft pause for the game

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
+    public static PauseManager Instance;
     public static bool IsPaused = false;
 
     public GameObject pausePanel;
 
     private float previousGameSpeed;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
@@ -17,18 +30,30 @@ public class PauseManager : MonoBehaviour
     public void TogglePause()
     {
         if (IsPaused)
+        {
             Resume();
+            if (pausePanel != null)
+            pausePanel.SetActive(false);
+        }
         else
+        {
             Pause();
+            if (pausePanel != null)
+            pausePanel.SetActive(true);
+        }
     }
 
     public void Pause()
     {
         IsPaused = true;
         previousGameSpeed = Time.timeScale;
-        Time.timeScale = 0f;
-        if (pausePanel != null)
-            pausePanel.SetActive(true);
+        // Time.timeScale = 0f;
+        Rigidbody2D[] allBodies = FindObjectsByType<Rigidbody2D>(FindObjectsSortMode.None);
+        foreach (var rb in allBodies) {
+            rb.simulated = false;
+        }
+        // if (pausePanel != null)
+        //     pausePanel.SetActive(true);
         GameInput.GameplayEnabled = false;
     }
 
@@ -36,6 +61,10 @@ public class PauseManager : MonoBehaviour
     {
         IsPaused = false;
         Time.timeScale = previousGameSpeed;
+        Rigidbody2D[] allBodies = FindObjectsByType<Rigidbody2D>(FindObjectsSortMode.None);
+        foreach (var rb in allBodies) {
+            rb.simulated = true;
+        }
         if (pausePanel != null)
             pausePanel.SetActive(false);
         GameInput.GameplayEnabled = true;
