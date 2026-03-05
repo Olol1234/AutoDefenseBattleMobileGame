0b62111fbb03debf49676a24e9aca754d51a8008
Olol1234 <muhammadazfar0239@gmail.com>
2026-03-05 23:03:03 +0800

    fix: make sure each shot only 1 sound called

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,12 +4,20 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
+    [Header("For tuning")]
+    private float lastTimeShoot;
+    [SerializeField] private float shootAudioCooldown = 0.05f;
+
     [Header("Audio Sources")]
     [SerializeField] private AudioSource sfxSource;
 
     [Header("Audio Clips")]
     public AudioClip clickSound;
     public AudioClip cancelSound;
+
+    [Header("Weapon Sounds")]
+    public AudioClip playerShootSound;
+    public AudioClip homingMissileSound;
 
     private void Awake()
     {
@@ -32,5 +40,22 @@ public class AudioManager : MonoBehaviour
     public void PlayCancel()
     {
         sfxSource.PlayOneShot(cancelSound);
+    }
+
+    public void PlayPlayerShoot()
+    {
+        if (Time.time - lastTimeShoot < shootAudioCooldown)
+        {
+            return;
+        }
+        lastTimeShoot = Time.time;
+        sfxSource.pitch = Random.Range(0.95f, 1.05f);
+        sfxSource.PlayOneShot(playerShootSound);
+    }
+
+    public void PlayHomingMissile()
+    {
+        sfxSource.pitch = Random.Range(0.95f, 1.05f);
+        sfxSource.PlayOneShot(homingMissileSound);
     }
 }
