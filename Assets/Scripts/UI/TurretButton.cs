ddbc0e5500feeeef8b10991c7b7fb64d89f457c8
Olol1234 <muhammadazfar0239@gmail.com>
2026-03-12 22:20:33 +0800

    feat: add image to block button if not yet unlocked

--- Assets/Scripts/UI/TurretButton.cs
+++ Assets/Scripts/UI/TurretButton.cs
@@ -5,10 +5,35 @@ public class TurretButtonUI : MonoBehaviour
 {
     public TurretType turretType;
     public TurretDetailPanel turretDetailPanel;
+    public int requiredStage;
+    public Image lockOverlay;
+
+    private void OnEnable()
+    {
+        CheckUnlockStatus();
+    }
 
     public void OnClick()
-    {
+    {   
+        if (lockOverlay.gameObject.activeSelf)
+        {
+            return;
+        }
         AudioManager.Instance.PlayClick();
         turretDetailPanel.Show(turretType);
+    }
+
+    public void CheckUnlockStatus()
+    {
+        var profile = PlayerProfile.Instance;
+
+        if (profile.highestStageCleared >= requiredStage)
+        {
+            lockOverlay.gameObject.SetActive(false);
+        }
+        else
+        {
+            lockOverlay.gameObject.SetActive(true);
+        }
     }
 }
