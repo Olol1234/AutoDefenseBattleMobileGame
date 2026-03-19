388e7931a8df6d8971280b26e052367cc397aab7
Olol1234 <muhammadazfar0239@gmail.com>
2026-03-19 18:22:03 +0800

    feat: script for balloon enemy

---
+++ Assets/Scripts/Enemy/EnemyBalloon.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyBalloon : MonoBehaviour, IKnockbackable
+{
+    [Header("Movement")]
+    public float speed = 2.5f;
+    private Vector2 knockbackVelocity;
+    [SerializeField] private float knockbackResist = 1f;
+    private float screenLimitX;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+
+
+    [Header("Spawn Settings")]
+    public GameObject[] minionPrefabs;
+    public int spawnCount = 3;
+    public float spawnSpread = 0.5f;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+        UpdateScreenLimits();
+    }
+
+    void UpdateScreenLimits()
+    {
+        if (mainCam == null) return;
+        float screenWidth = (mainCam.orthographicSize * 2f) * mainCam.aspect;
+        screenLimitX = (screenWidth / 2f) - 0.4f;
+    }
+
+    public void ApplyKnockback(Vector2 pushDirection, float force)
+    {
+        knockbackVelocity += pushDirection.normalized * force;
+    }
+
+    void FixedUpdate()
+    {
+        if (PauseManager.IsPaused) 
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+
+        knockbackVelocity = Vector2.Lerp(knockbackVelocity, Vector2.zero, knockbackResist * Time.fixedDeltaTime);
+
+        rb.linearVelocity = (Vector2.down * speed) + knockbackVelocity;
+
+        UpdateScreenLimits();
+        float clampedX = Mathf.Clamp(rb.position.x, -screenLimitX, screenLimitX);
+        rb.position = new Vector2(clampedX, rb.position.y);
+    }
+
+    public void OnDeath()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            int randomIndex = Random.Range(0, minionPrefabs.Length);
+            
+            Vector3 spawnOffset = new Vector3(Random.Range(-spawnSpread, spawnSpread), 0, 0);
+
+            GameObject minion = ObjectPooler.Instance.GetFromPool(
+                minionPrefabs[randomIndex], 
+                transform.position + spawnOffset, 
+                Quaternion.identity
+            );
+        }
+        
+        // Return the Balloon itself to the pool
+        gameObject.SetActive(false);
+    }
+
+}
